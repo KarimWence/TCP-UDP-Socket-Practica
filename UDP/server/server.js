2f07f53c4829ad7bb22e9db4d64329ec194fc59c
YahirRadilla <yahirradi09@gmail.com>
2026-03-19 14:58:05 -0700

    udp

---
+++ UDP/server/server.js
@@ -0,0 +1,29 @@
+const dgram = require("dgram");
+
+const servidor = dgram.createSocket("udp4");
+
+const PUERTO = 41234;
+
+servidor.on("message", (msg, rinfo) => {
+  console.log(`Mensaje recibido: ${msg}`);
+  console.log(`De: ${rinfo.address}:${rinfo.port}`);
+
+  const respuesta = Buffer.from(`Mensaje: ${msg} \n recibido correctamente`);
+
+  servidor.send(respuesta, rinfo.port, rinfo.address, (err) => {
+    if (err) {
+      console.error("Error al enviar respuesta:", err);
+    } else {
+      console.log("Respuesta enviada al cliente\n");
+    }
+  });
+});
+
+servidor.on("listening", () => {
+  const direccion = servidor.address();
+  console.log(
+    `Servidor UDP escuchando en ${direccion.address}:${direccion.port}`,
+  );
+});
+
+servidor.bind(PUERTO);
