c87ea9ad290fc4490ec3e4ad9191724fe7d488e5
Karim Dasaev Wence Landin <kwence_22@alu.uabcs.mx>
2026-03-19 15:36:59 -0700

    update tcp client and server files

--- TCP/server/server.js
+++ TCP/server/server.js
@@ -4,34 +4,42 @@ const PORT = 12345;
 let clients = [];
 
 const server = net.createServer((socket) => {
-  console.log('Cliente conectado');
+    console.log('Cliente conectado');
 
-  clients.push(socket);
+    clients.push(socket);
 
-  socket.on('data', (data) => {
-    const message = data.toString().trim();
+    socket.on('data', (data) => {
+        const message = data.toString().trim();
 
-    console.log('Mensaje recibido:', message);
+        console.log('Mensaje recibido:', message);
 
-    // Enviar a todos los clientes excepto el emisor
-    clients.forEach((client) => {
-      if (client !== socket) {
-        client.write(message + '\n');
-      }
+        // Enviar a todos los clientes excepto el emisor
+        clients.forEach((client) => {
+            if (client !== socket) {
+                client.write(message + '\n');
+            }
+        });
     });
-  });
 
-  socket.on('end', () => {
-    console.log('Cliente desconectado');
-    clients = clients.filter((client) => client !== socket);
-  });
+    socket.on('end', () => {
+        console.log('Cliente desconectado');
+        clients = clients.filter((client) => client !== socket);
+    });
 
-  socket.on('error', (err) => {
-    console.log('Error:', err.message);
-  });
+    socket.on('close', () => {
+        clients = clients.filter((client) => client !== socket);
+    });
+
+    socket.on('error', (err) => {
+        if (err.code === 'ECONNRESET') {
+            console.log('Cliente desconectado abruptamente');
+        } else {
+            console.log('Error:', err.message);
+        }
+    });
 });
 
 // IMPORTANTE: permitir conexiones externas
 server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Servidor escuchando en puerto ${PORT}`);
+    console.log(`Servidor escuchando en puerto ${PORT}`);
 });
